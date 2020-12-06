9735756972a45e871bd7bff36c52b48428936903
Sektor <krzysiek1230@gmail.com>
2020-12-06 10:42:39 +0100

    Add connection to postgres

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import { Client } from "pg";
+import { APP_CONFIG } from "./utils/appConfig";
+
+const client = new Client({
+  host: APP_CONFIG.POSTGRES_HOST,
+  user: APP_CONFIG.POSTGRES_USER,
+  password: APP_CONFIG.POSTGRES_PASSWORD,
+  port: parseInt(APP_CONFIG.POSTGRES_PORT),
+});
+
+client
+  .connect()
+  .then(() =>
+    console.log(`Connected to Postgres at port ${APP_CONFIG.POSTGRES_PORT}`)
+  )
+  .catch((e) => console.log(e));
---
+++ src/utils/appConfig.ts
@@ -0,0 +1,16 @@
+import dotenv, { DotenvConfigOutput } from "dotenv";
+
+interface AppConfig {
+  POSTGRES_HOST: string;
+  POSTGRES_PORT: string;
+  POSTGRES_USER: string;
+  POSTGRES_PASSWORD: string;
+}
+
+const processedConfig: DotenvConfigOutput = dotenv.config();
+
+if (processedConfig.error && !processedConfig.parsed) {
+  throw new Error("There was an error during environmental variables parsing!");
+}
+
+export const APP_CONFIG = (dotenv.config().parsed as unknown) as AppConfig;
